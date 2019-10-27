e0947ed8ba4b3c00cc0cd24f4e658a00d64f2a9f
Marc Jacques <Marcjacques@me.com>
2019-10-27 12:14:15 -0400

    Turned VC into TVC. Created TVC in storyboard.

--- StormViewer/ViewControllers/StormViewerTableViewController.swift
+++ StormViewer/ViewControllers/StormViewerTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  StormViewerTableViewController.swift
+//  ViewController.swift
 //  StormViewer
 //
 //  Created by Marc Jacques on 10/26/19.
@@ -9,82 +9,28 @@
 import UIKit
 
 class StormViewerTableViewController: UITableViewController {
-
+    
+    // MARK: - Properties
+    var pictures = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    
+        let fm = FileManager.default //fileManager allows us to work through the filesystem and will allow us to look through the files in this app.
+        let path = Bundle.main.resourcePath!// this lets us have a direct path to our file bundle given from the file manager
+        let items = try! fm.contentsOfDirectory(atPath: path)// items allows us to work with the selected file manager and our specifc path to begin going through the files
+        
+        for items in items {
+            if items.hasPrefix("nssl") {
+                //this for loop loops through all the files in our desired path to load the pictures
+                //the if clause helps locate the specifc files that  have a specific prefix
+                pictures.append(items)
+                print(pictures)
+            }
+        }
+        // Do any additional setup after loading the view.
     }
 
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
